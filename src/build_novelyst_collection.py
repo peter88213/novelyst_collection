647696ba027b7e4d79aedc5a17d1eafe831fbd30
peter88213 <peter.88213@t-online.de>
2022-11-01 21:37:20 +0100

    Set up build/localization scripts

---
+++ src/build_novelyst_collection.py
@@ -0,0 +1,30 @@
+""" Build a collection  novelyst plugin.
+        
+In order to distribute a single script without dependencies, 
+this script "inlines" all modules imported from the pywriter package.
+
+The PyWriter project (see see https://github.com/peter88213/PyWriter)
+must be located on the same directory level as the collectionyw project. 
+
+For further information see https://github.com/peter88213/novelyst_collection
+License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+"""
+import os
+import sys
+sys.path.insert(0, f'{os.getcwd()}/../../PyWriter/src')
+import inliner
+
+SRC = '../src/'
+BUILD = '../test/'
+SOURCE_FILE = f'{SRC}novelyst_collection.py'
+TARGET_FILE = f'{BUILD}novelyst_collection.py'
+
+
+def main():
+    inliner.run(SOURCE_FILE, TARGET_FILE, 'nvcollectionlib', '../../novelyst_collection/src/')
+    inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/')
+    print('Done.')
+
+
+if __name__ == '__main__':
+    main()
